6bc876216c35ab1e7987da6732bac3d5c64dd134
Michal Zurawski <michal.zurawski@ntti3.com>
2017-09-28 11:25:52 +0200

    Added missing licences

--- extension/goplugin/util.go
+++ extension/goplugin/util.go
@@ -1,14 +1,26 @@
+// Copyright (C) 2017 NTT Innovation Institute, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package goplugin
 
 import "github.com/golang/mock/gomock"
 
-var controllers map[gomock.TestReporter]*gomock.Controller
+var controllers map[gomock.TestReporter]*gomock.Controller = make(map[gomock.TestReporter]*gomock.Controller)
 
 func NewController(testReporter gomock.TestReporter) *gomock.Controller {
 	ctrl := gomock.NewController(testReporter)
-	if controllers == nil {
-		controllers = make(map[gomock.TestReporter]*gomock.Controller)
-	}
 	controllers[testReporter] = ctrl
 	return ctrl
 }
